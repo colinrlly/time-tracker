b2c260471a6722f8cad761cccdce3e477230c47a
colin reilly <colinrlly@gmail.com>
2018-05-25 10:23:04 -0400

    Get hello world from Jinja template

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    """ Renders the homepage template """
+    return render_template("index.html")
