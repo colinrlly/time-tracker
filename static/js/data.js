a871ca3df4a8b478762711c78801f94ddcb569a9
colin reilly <colinrlly@gmail.com>
2019-08-01 14:19:02 -0400

    Created initial custom legend.

--- static/js/data.js
+++ static/js/data.js
@@ -3,50 +3,51 @@ var date_ranges = [];
 
 // Aggregates and formats a list of activity records which usually comes from the server
 function format_events(res) {
-        // Get the duration of each event.
-        var events = res.list.map(function (x) {
-            var end = moment(x.end.dateTime);
-            var start = moment(x.start.dateTime);
+    // Get the duration of each event.
+    var events = res.list.map(function (x) {
+        var end = moment(x.end.dateTime);
+        var start = moment(x.start.dateTime);
 
-            return {
-                'duration': end.diff(start, 'minutes'),
-                'name': x['summary'],
-                'colorId': x['colorId'],
-                'inActivities': x['inActivities']
+        return {
+            'duration': end.diff(start, 'minutes'),
+            'name': x['summary'],
+            'colorId': x['colorId'],
+            'inActivities': x['inActivities']
+        };
+    });
+
+    // Aggregate the events together grouped by name.
+    var agg_events = events.reduce(function (acc, curr) {
+        var curr_name = curr['name'];
+
+        if (!acc[curr_name]) {
+            acc[curr_name] = {
+                'duration': 0,
+                'colorId': curr['colorId'],
+                'inActivities': curr['inActivities']
             };
-        });
-
-        // Aggregate the events together grouped by name.
-        var agg_events = events.reduce(function (acc, curr) {
-            var curr_name = curr['name'];
-
-            if (!acc[curr_name]) {
-                acc[curr_name] = {
-                    'duration': 0,
-                    'colorId': curr['colorId'],
-                    'inActivities': curr['inActivities']};
-            }
-
-            acc[curr_name]['duration'] += curr['duration'];
-
-            return acc;
-        }, {});
-
-        // Turn the dictionary of aggregated events into an array.
-        var agg_events_array = [];
-        for (var event in agg_events) {
-            agg_events_array.push({
-                'name': event,
-                'duration': (agg_events[event]['duration'] / 60).toFixed(2),
-                'colorId': agg_events[event]['colorId'],
-                'inActivities': agg_events[event]['inActivities']
-            });
         }
 
-        // Sort the array so the events show up in order of increasing duration.
-        agg_events_array.sort(function(a, b) { return b['duration'] - a['duration'] });
+        acc[curr_name]['duration'] += curr['duration'];
 
-        return agg_events_array;
+        return acc;
+    }, {});
+
+    // Turn the dictionary of aggregated events into an array.
+    var agg_events_array = [];
+    for (var event in agg_events) {
+        agg_events_array.push({
+            'name': event,
+            'duration': (agg_events[event]['duration'] / 60).toFixed(2),
+            'colorId': agg_events[event]['colorId'],
+            'inActivities': agg_events[event]['inActivities']
+        });
+    }
+
+    // Sort the array so the events show up in order of increasing duration.
+    agg_events_array.sort(function (a, b) { return b['duration'] - a['duration'] });
+
+    return agg_events_array;
 }
 
 // Function to run when a custom date range is selected.
@@ -68,7 +69,7 @@ $(document).ready(function () {
         opens: 'left',
         startDate: moment().subtract(7, 'd').format('MM/DD/YYYY'),
         endDate: moment().format('MM/DD/YYYY')
-    }, function(start, end, label) {
+    }, function (start, end, label) {
         $('input[name="daterange"]').data('start', start);
         $('input[name="daterange"]').data('end', end);
 
@@ -76,7 +77,7 @@ $(document).ready(function () {
 
         updateChart();
     })
-    
+
     // Set initial date range data
     $('input[name="daterange"]').data('start', moment().subtract(7, 'd'));
     $('input[name="daterange"]').data('end', moment());
@@ -107,8 +108,44 @@ $(document).ready(function () {
                 // These labels appear in the legend and in the tooltips when hovering different arcs
                 labels: agg_events_array.map(function (x) { return x.name; })
             },
-            options: {}
+            options: {
+                legend: false,  // Don't show the default legend
+                legendCallback: function (chart) {  // Instead create our own custom legend
+                    var legendHtml = [];
+                    legendHtml.push('<ul class="chartLegend">');
+
+                    var item = chart.data.datasets[0];
+
+                    for (var i = 0; i < item.data.length; i++) {
+                        legendHtml.push('<li>');
+                        legendHtml.push('<span class="chartLegendLabel" style="background-color:' + item.backgroundColor[i] + '">' + item.data[i] + ' hours - ' + chart.data.labels[i] + '</span>');
+                        legendHtml.push('</li>');
+                    }
+
+                    legendHtml.push('</ul>');
+
+                    return legendHtml.join("");
+                }
+            }
         });
+
+        // Show / hide data slice on label click
+        $(".chartLegend").on('click', "li", function() {
+            var index = $(this).index();
+
+            if (pieChart.getDatasetMeta(0).data[index].hidden) {
+                pieChart.getDatasetMeta(0).data[index].hidden = false;
+                $(this).css('text-decoration', 'none');
+            } else {
+                pieChart.getDatasetMeta(0).data[index].hidden = true;
+                $(this).css('text-decoration', 'line-through');
+            }
+                            
+            // We hid a dataset ... rerender the chart
+            pieChart.update();
+        })
+
+        $('.chartLegend').html(pieChart.generateLegend());  // Call this to generate our own legend
     });
 })
 
@@ -118,7 +155,7 @@ function updateChart() {
     // This array is used to keep track of the last selected date range,
     // so if the data returns from the server out of order this array is used to
     // update using only the most recent date range.
-    date_ranges.push({'start': getStartOfRange().toISOString(), 'end': getEndOfRange().toISOString()});
+    date_ranges.push({ 'start': getStartOfRange().toISOString(), 'end': getEndOfRange().toISOString() });
 
     // Ask the server for the events in a specified date range.
     $.post('/api/list_events', {
@@ -160,10 +197,10 @@ $('.rangeTypeBtn').click(function () {
 })
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (!event.target.matches('.rangeTypeBtn')
         && $('.rangeTypeBtn').is(':visible')) {
-            $('.rangeTypeDropdownContent').hide();
+        $('.rangeTypeDropdownContent').hide();
     }
 }
 
@@ -173,7 +210,7 @@ $('.rangeTypeDropdownContent button').click(function (event) {
 
     $('.rangeTypeBtn').html(selection);
 
-    switch(selection) {
+    switch (selection) {
         case 'Day':
             setRange(getEndOfRange(), 1);
             $('input[name="daterange"]').data('rangeSize', 1);
