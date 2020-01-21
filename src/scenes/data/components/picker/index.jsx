6902d1a33b5d810aaf016e31f62f8b89b95f4024
Colin Reilly <johndoe@example.com>
2020-01-21 10:41:44 -0500

    Backward and forwards buttons work.

--- src/scenes/data/components/picker/index.jsx
+++ src/scenes/data/components/picker/index.jsx
@@ -5,31 +5,89 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-daterangepicker/daterangepicker.css';
 
 class Picker extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            startDateTime: moment().subtract(6, 'days'),
+            endDateTime: moment(),
+        };
+    }
+
+    handleApplyEvent(picker) {
+        console.log(picker);
+
+        this.setState({
+            startDateTime: picker.startDate,
+            endDateTime: picker.endDate,
+        });
+    }
+
+    handleRangeDirectionButton(direction) {
+        const { startDateTime, endDateTime } = this.state;
+
+        const duration = moment.duration(endDateTime.diff(startDateTime));
+        const durationAsDays = Math.floor(duration.asDays()) + 1;
+
+        switch (direction) {
+            case 'backward': {
+                const newEnd = endDateTime.subtract(durationAsDays, 'days');
+                const newStart = startDateTime.subtract(durationAsDays, 'days');
+
+                this.setState({
+                    startDateTime: newStart,
+                    endDateTime: newEnd,
+                });
+
+                break;
+            }
+
+            case 'forward': {
+                const newEnd = endDateTime.add(durationAsDays, 'days');
+                const newStart = startDateTime.add(durationAsDays, 'days');
+
+                this.setState({
+                    startDateTime: newStart,
+                    endDateTime: newEnd,
+                });
+                break;
+            }
+
+            default:
+                break;
+        }
+    }
+
     render() {
-        this;
+        const { startDateTime, endDateTime } = this.state;
+        const buttonText = `${startDateTime.format('M/D/YY hh:mm a')} ${endDateTime.format('M/D/YY hh:mm a')}`;
 
         return (
             <div>
-                <button class="rangeBackwardBtn">{'<'}</button>
+                <button onClick={() => this.handleRangeDirectionButton('backward')}>{'<'}</button>
+
                 <DateRangePicker
-                    startDate="1/1/2014"
-                    endDate="3/1/2014"
+                    startDate={startDateTime}
+                    endDate={endDateTime}
                     ranges={{
                         Today: [moment(), moment()],
                         Yesterday: [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
                         'Last 7 Days': [moment().subtract(6, 'days'), moment()],
                         'Last 30 Days': [moment().subtract(29, 'days'), moment()],
                         'This Month': [moment().startOf('month'), moment().endOf('month')],
-                        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
                     }}
                     timePicker={true}
                     locale={{
                         format: 'M/DD hh:mm A',
                     }}
+                    onApply={(event, picker) => this.handleApplyEvent(picker)}
+                    alwaysShowCalendars={true}
                 >
-                    <button>Click Me To Open Picker!</button>
+                    <button>{buttonText}</button>
                 </DateRangePicker>
-                <button class="rangeBackwardBtn">{'>'}</button>
+
+                <button onClick={() => this.handleRangeDirectionButton('forward')}>{'>'}</button>
             </div >
         );
     }
