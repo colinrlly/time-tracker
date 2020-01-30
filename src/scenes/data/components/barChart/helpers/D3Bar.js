f046578bf6ba5a0e92ea993232b9d66a2cb03228
Colin Reilly <johndoe@example.com>
2020-01-30 14:15:30 -0500

    For desktop.

--- src/scenes/data/components/barChart/helpers/D3Bar.js
+++ src/scenes/data/components/barChart/helpers/D3Bar.js
@@ -2,109 +2,151 @@ import * as d3 from 'd3';
 
 const D3Bar = {};
 
+var myData = [21, 3, 5, 21, 15];
+
 D3Bar.create = function create(svgNode, data, configuration) {
     // D3 code to create the bar chart
-    const svg = d3.select(svgNode);
+    var xScale = d3.scale.ordinal()
+        .domain(d3.range(myData.length))
+        .rangeRoundBands([0, w], 0.05);
 
-    const margin = 80;
-    const width = 1000 - 2 * margin;
-    const height = 600 - 2 * margin;
+    let yScale = d3.scale.linear()
+        .domain([0, d3.max(myData)])
+        .range([0, h]);
 
-    const chart = svg.append('g')
-        .attr('transform', `translate(${margin}, ${margin})`);
+    let svg = d3.select(svgNode);
 
-    const xScale = d3.scaleBand()
-        .range([0, width])
-        .domain(data.map((s) => s.name))
-        .padding(0.4);
-
-    const yScale = d3.scaleLinear()
-        .range([height, 0])
-        .domain([0, 100]);
-
-    // vertical grid lines
-    // const makeXLines = () => d3.axisBottom()
-    //   .scale(xScale)
-
-    const makeYLines = () => d3.axisLeft()
-        .scale(yScale)
-
-    chart.append('g')
-        .attr('transform', `translate(0, ${height})`)
-        .call(d3.axisBottom(xScale));
-
-    chart.append('g')
-        .call(d3.axisLeft(yScale));
-
-    // vertical grid lines
-    // chart.append('g')
-    //   .attr('class', 'grid')
-    //   .attr('transform', `translate(0, ${height})`)
-    //   .call(makeXLines()
-    //     .tickSize(-height, 0, 0)
-    //     .tickFormat('')
-    //   )
-
-    chart.append('g')
-        .attr('class', 'grid')
-        .call(makeYLines()
-            .tickSize(-width, 0, 0)
-            .tickFormat(''));
-
-    const barGroups = chart.selectAll()
-        .data(data)
+    svg.selectAll("rect")
+        .data(myData)
         .enter()
-        .append('g');
+        .append("rect")
+        .attr("x", function (d, i) {
+            return xScale(i);
+        })
+        .attr("y", function (d) {
+            return h - yScale(d);
+        })
+        .attr("width", xScale.rangeBand())
+        .attr("height", function (d) {
+            return yScale(d);
+        })
+        .attr("fill", "steelblue");
 
-    barGroups
-        .append('rect')
-        .attr('class', 'bar')
-        .attr('x', (g) => xScale(g.name))
-        .attr('y', (g) => yScale(g.duration))
-        .attr('height', (g) => height - yScale(g.duration))
-        .attr('width', xScale.bandwidth());
+    // Select the svg element.
+    // const svg = d3.select(svgNode);
 
-    barGroups
-        .append('text')
-        .attr('class', 'duration')
-        .attr('x', (a) => xScale(a.name) + xScale.bandwidth() / 2)
-        .attr('y', (a) => yScale(a.duration) + 30)
-        .attr('text-anchor', 'middle')
-        .text((a) => `${a.duration}%`);
+    // // Add margin and padding to svg.
+    // const chart = svg.append('g')
+    //     .attr('transform', `translate(${configuration.margin}, ${configuration.margin})`);
 
-    svg
-        .append('text')
-        .attr('class', 'label')
-        .attr('x', -(height / 2) - margin)
-        .attr('y', margin / 2.4)
-        .attr('transform', 'rotate(-90)')
-        .attr('text-anchor', 'middle')
-        .text('Love meter (%)');
+    // // Scale the x axis.
+    // const xScale = d3.scaleBand() // Scale band splits the range into bands.
+    //     .range([0, configuration.width])
+    //     .domain(data.map((s) => s.name))
+    //     .padding(0.4);
 
-    svg.append('text')
-        .attr('class', 'label')
-        .attr('x', width / 2 + margin)
-        .attr('y', height + margin * 1.7)
-        .attr('text-anchor', 'middle')
-        .text('names');
+    // // Scale the y axis.
+    // const yScale = d3.scaleLinear()
+    //     .range([configuration.height, 0])
+    //     .domain([0, 100]);
 
-    svg.append('text')
-        .attr('class', 'title')
-        .attr('x', width / 2 + margin)
-        .attr('y', 40)
-        .attr('text-anchor', 'middle')
-        .text('Most loved programming names in 2018');
+    // Create the axis.
+    // chart.append('g')
+    //     .attr('transform', `translate(0, ${configuration.height})`)
+    //     .call(d3.axisBottom(xScale));
+    // chart.append('g')
+    //     .call(d3.axisLeft(yScale));
 
-    svg.append('text')
-        .attr('class', 'source')
-        .attr('x', width - margin / 2)
-        .attr('y', height + margin * 1.7)
-        .attr('text-anchor', 'start')
-        .text('Source: Stack Overflow, 2018');
+    // // Create the lines on the left axis.
+    // const makeYLines = () => d3.axisLeft()
+    //     .scale(yScale);
+    // chart.append('g')
+    //     .attr('class', 'grid')
+    //     .call(makeYLines()
+    //         .tickSize(-configuration.width, 0, 0)
+    //         .tickFormat(''));
+
+    // Create the element to represent the bars in the chart.
+    // const barGroups = chart.selectAll('rect')
+    //     .data(data)
+    //     .enter()
+    //     .append('rect');
+
+    // // Give the bars in the chart height and width.
+    // barGroups
+    //     .append('rect')
+    //     // .attr('class', 'bar')
+    //     .attr('x', (g) => xScale(g.name))
+    //     .attr('y', (g) => yScale(g.duration))
+    //     .attr('height', (g) => configuration.height - yScale(g.duration))
+    //     .attr('width', xScale.bandwidth())
+    //     .attr('fill', 'steelblue');
+
+    // // Give the bars text
+    // barGroups
+    //     .append('text')
+    //     .attr('class', 'duration')
+    //     .attr('x', (a) => xScale(a.name) + xScale.bandwidth() / 2)
+    //     .attr('y', (a) => yScale(a.duration) + 30)
+    //     .attr('text-anchor', 'middle')
+    //     .text((a) => `${a.duration}%`);
+
+    // // Create left text label.
+    // svg.append('text')
+    //     .attr('class', 'label')
+    //     .attr('x', -(configuration.height / 2) - configuration.margin)
+    //     .attr('y', configuration.margin / 2.4)
+    //     .attr('transform', 'rotate(-90)')
+    //     .attr('text-anchor', 'middle')
+    //     .text('Love meter (%)');
+
+    // // Create bottom text label.
+    // svg.append('text')
+    //     .attr('class', 'label')
+    //     .attr('x', configuration.width / 2 + configuration.margin)
+    //     .attr('y', configuration.height + configuration.margin * 1.7)
+    //     .attr('text-anchor', 'middle')
+    //     .text('names');
+
+    // // Create the top label.
+    // svg.append('text')
+    //     .attr('class', 'title')
+    //     .attr('x', configuration.width / 2 + configuration.margin)
+    //     .attr('y', 40)
+    //     .attr('text-anchor', 'middle')
+    //     .text('Most loved programming names in 2018');
+
+    // // Create bottom right text label.
+    // svg.append('text')
+    //     .attr('class', 'source')
+    //     .attr('x', configuration.width - configuration.margin / 2)
+    //     .attr('y', configuration.height + configuration.margin * 1.7)
+    //     .attr('text-anchor', 'start')
+    //     .text('Source: Stack Overflow, 2018');
+
+    return {
+        chart,
+        xScale,
+        yScale,
+    };
 };
 
-D3Bar.update = function update(element, data, configuration, chart) {
-    // D3 code to update the chart
+D3Bar.update = function update(element, newData, configuration, chart, xScale, yScale) {
+    const barGroups = chart.selectAll('rect')
+        .data(newData);
+
+    barGroups.enter()
+        .append('rect');
+
+    barGroups.transition()
+        .duration(300)
+        .attr('x', (g) => xScale(g.name))
+        .attr('y', (g) => yScale(g.duration))
+        .attr('height', (g) => configuration.height - yScale(g.duration))
+        .attr('width', xScale.bandwidth());
+
+    barGroups.exit()
+        .remove();
 };
 
 D3Bar.destroy = function destroy() {
--- src/scenes/data/components/barChart/index.jsx
+++ src/scenes/data/components/barChart/index.jsx
@@ -3,22 +3,37 @@ import { connect } from 'react-redux';
 import { D3Bar } from './helpers';
 
 class BarChart extends Component {
-    // componentDidMount() {
-    //     // D3 cod eto create the chart
-    //     this.chart = D3Bar.create(
-    //         this.refs.svgNode,
-    //         this.props.data,
-    //         this.props.config,
-    //     );
-    // }
+    componentDidMount() {
+        // Define size parameters.
+        const margin = 80;
+
+        this.configuration = {
+            margin,
+            width: 1000 - 2 * margin,
+            height: 600 - 2 * margin,
+        };
+
+        // D3 code to create the chart
+        const { chart, xScale, yScale } = D3Bar.create(
+            this.refs.svgNode,
+            this.props.data,
+            this.configuration,
+        );
+
+        this.chart = chart;
+        this.xScale = xScale;
+        this.yScale = yScale;
+    }
 
     componentDidUpdate() {
         // D3 code to update the chart
-        D3Bar.create(
+        D3Bar.update(
             this.refs.svgNode,
             this.props.data,
-            this.props.config,
-            // this.chart,
+            this.configuration,
+            this.chart,
+            this.xScale,
+            this.yScale,
         );
     }
 
