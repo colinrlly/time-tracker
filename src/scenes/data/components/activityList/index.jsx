1a39797985cdf5a8f37e020ffc9a5f56902b7f81
Colin Reilly <johndoe@example.com>
2020-03-02 19:21:17 -0500

    Can select and unselect activity names.

--- src/scenes/data/components/activityList/index.jsx
+++ src/scenes/data/components/activityList/index.jsx
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import {
+    setActivityNames,
+} from '../../../../redux/actions/actions';
 
 function ActivityList(props) {
     const inActivitiesNames = Object.keys(props.names).filter(
         (name) => props.names[name].inActivities,
     );
-
     const notInActivitiesNames = Object.keys(props.names).filter(
         (name) => !props.names[name].inActivities,
     );
 
-    const inActivitiesList = inActivitiesNames.map((name, i) => <li key={i}>{name}</li>);
-    const notInActivitiesList = notInActivitiesNames.map((name, i) => <li key={i}>{name}</li>);
+    function handleNameClick(name) {
+        const newNames = JSON.parse(JSON.stringify(props.names));
+
+        newNames[name].selected = !newNames[name].selected;
+
+        props.setActivityNames(newNames);
+    }
+
+    const selectedStyle = {
+        border: '1px solid red',
+    };
+
+    const inActivitiesList = inActivitiesNames.map(
+        (name, i) => <li key={i}>
+            <button
+                onClick={() => handleNameClick(name)}
+                style={props.names[name].selected ? selectedStyle : null}>
+                {name}
+            </button>
+        </li>,
+    );
+    const notInActivitiesList = notInActivitiesNames.map(
+        (name, i) => <li key={i}>
+            <button
+                onClick={() => handleNameClick(name)}
+                style={props.names[name].selected ? selectedStyle : null}>
+                {name}
+            </button>
+        </li>,
+    );
 
     return (
         <div>
@@ -31,8 +61,12 @@ const mapStateToProps = (state) => ({
     names: state.names,
 });
 
+const mapDispatchToProps = {
+    setActivityNames,
+};
+
 ActivityList.propTypes = {
     prop: PropTypes,
 };
 
-export default connect(mapStateToProps, null)(ActivityList);
+export default connect(mapStateToProps, mapDispatchToProps)(ActivityList);
