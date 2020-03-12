5e3a182c6d9523cd27fb52d7928ac8f6fe67012e
Colin Reilly <johndoe@example.com>
2020-03-12 16:55:30 -0600

    Set up sass and css modules?? Oh webpack...

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,46 @@
 const webpack = require('webpack');
 const resolve = require('path').resolve;
+const autoprefixer = require('autoprefixer');
+
+const CSSModuleLoader = {
+    loader: 'css-loader',
+    options: {
+        modules: {
+            localIdentName: '[local]__[hash:base64:5]',
+        },
+        sourceMap: true,
+    },
+};
+
+const CSSLoader = {
+    loader: 'css-loader',
+    options: {
+        modules: false,
+        sourceMap: true,
+    },
+};
+
+const postCSSLoader = {
+    loader: 'postcss-loader',
+    options: {
+        ident: 'postcss',
+        sourceMap: true,
+        plugins: () => [
+            autoprefixer(),
+        ],
+    },
+};
+
 const config = {
     devtool: 'eval-source-map',
     entry: __dirname + '/src/index.jsx',
     output: {
         path: resolve('./src/static/build'),
         filename: 'bundle.js',
-        publicPath: resolve('./build`')
+        publicPath: resolve('./build`'),
     },
     resolve: {
-        extensions: ['.js', '.jsx', '.css']
+        extensions: ['.js', '.jsx', '.css'],
     },
     module: {
         rules: [
@@ -18,13 +49,24 @@ const config = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
-                    presets: ['@babel/preset-react', '@babel/preset-env']
-                }
+                    presets: ['@babel/preset-react', '@babel/preset-env'],
+                },
             },
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
-            }]
-    }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /\.module\.scss$/,
+                use: ['style-loader', CSSLoader, postCSSLoader, 'sass-loader'],
+            },
+            {
+                test: /\.module\.scss$/,
+                use: ['style-loader', CSSModuleLoader, postCSSLoader, 'sass-loader'],
+            },
+        ],
+    },
 };
+
 module.exports = config;
