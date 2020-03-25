3db912911e432e56ab18779be85776532de4af07
colin reilly <colinrlly@gmail.com>
2020-03-25 11:51:39 -0600

    Styled total hours.

--- src/scenes/data/components/totalTime/index.jsx
+++ src/scenes/data/components/totalTime/index.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import {
+    totalTime,
+    totalTimeLabel,
+} from './style/style.module.scss';
+
 function TotalTime(props) {
-    return <h1>{(props.totalTime / 60).toFixed(2)}</h1>;
+    return (
+        <div>
+            <h1 className={totalTime}>{props.totalTime.toFixed(2)}</h1>
+            <p className={totalTimeLabel}>TOTAL HOURS</p>
+        </div>
+    );
 }
 
 const mapStateToProps = (state) => ({
