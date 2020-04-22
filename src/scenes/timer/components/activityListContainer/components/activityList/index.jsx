fec27b26b6290c9aa99751e3e520d594c38ebbf1
colin reilly <colinrlly@gmail.com>
2020-04-22 15:59:02 -0600

    Initial activityList styles.

--- src/scenes/timer/components/activityListContainer/components/activityList/index.jsx
+++ src/scenes/timer/components/activityListContainer/components/activityList/index.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { googleColorsRGB } from '../../../../../../static/js/google_colors';
+
+import style from './style/style.module.scss';
+
 function ActivityList(props) {
     const activityComponents = props.activities.map((x, i) => (
-        <li key={i}>
+        <li
+            key={i}
+            className={style.activityListItem}
+            style={{
+                backgroundColor:
+                    `rgba(
+                        ${googleColorsRGB[x.color][0]},
+                        ${googleColorsRGB[x.color][1]},
+                        ${googleColorsRGB[x.color][2]},
+                        1
+                    )`,
+            }}
+        >
             <button
                 onClick={() => props.handleActivityClick(x)}
                 disabled={props.disabled}
+                className={style.activityListBtn}
             >{x.name}</button>
-            <button
+            {/* <button
                 onClick={() => props.editCallback(x)}
                 data-testid={`edit-${x.name}`}
-            >edit</button>
+                className={style.activityListEditBtn}
+            >edit</button> */}
         </li>));
 
     return (
-        <ul>
+        <ul className={style.activityList}>
             {
                 (props.activityIsRunning || props.hasUnsavedActivityRecord)
                     ? null
