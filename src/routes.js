b1ea12a3140a0e6116d201fb0b36c55473fb64ef
Colin Reilly <colinrlly@gmail.com>
2021-02-11 17:51:08 -0700

    Creates checkout session and routes to stripe for payment.

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
 import store from './redux/store/store';
 import Data from './scenes/data';
 import Timer from './scenes/timer';
@@ -8,16 +11,22 @@ import Landing from './scenes/landing';
 import PremiumLanding from './scenes/premiumLanding';
 import { Socket } from './components';
 
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe('pk_test_Dt7r6xru8VEdvmTAkELr3zGg00XLII0yUP');
+
 export default (
-    <Provider store={store}>
-        <Socket />
-        <BrowserRouter>
-            <Switch>
-                <Route exact path='/' component={Timer} />
-                <Route path='/data' component={Data} />
-                <Route path='/landing' component={Landing} />
-                <Route path='/premium' component={PremiumLanding} />
-            </Switch>
-        </BrowserRouter>
-    </Provider>
+    <Elements stripe={stripePromise}>
+        <Provider store={store}>
+            <Socket />
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' component={Timer} />
+                    <Route path='/data' component={Data} />
+                    <Route path='/landing' component={Landing} />
+                    <Route path='/premium' component={PremiumLanding} />
+                </Switch>
+            </BrowserRouter>
+        </Provider>
+    </Elements>
 );
--- src/scenes/premiumLanding/index.jsx
+++ src/scenes/premiumLanding/index.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import {
+    useStripe,
+} from '@stripe/react-stripe-js';
+
+const STRIPE_PREMIUM_PRICE_ID = 'price_1IGvSCLWafTZTzjvegopKTzn';
 
 function PremiumLanding() {
+    const stripe = useStripe();
+
+    function createCheckoutSession(priceId) {
+        return fetch('/create-checkout-session', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                priceId,
+            }),
+        }).then((result) => result.json());
+    }
+
     return (
-        <div>
-            This is premium landing
-        </div>
-    )
+        <button
+            onClick={() => {
+                createCheckoutSession(STRIPE_PREMIUM_PRICE_ID).then((data) => {
+                    console.log(data.sessionId);
+
+                    stripe.redirectToCheckout({
+                        sessionId: data.sessionId,
+                    }).then((result) => console.log(result));
+                });
+            }}
+        >Subscribe</button>
+    );
 }
 
 export default PremiumLanding;
